1e673b001fe6e00ee97717d98dbef63506e08a40
Vadim Kravchenko <kravchenko.vadim.dev@gmail.com>
2026-01-03 16:20:59 +0200

    less.176.1

--- app/src/main/java/com/kravchenkovadim/movies/activities/MainActivity.java
+++ app/src/main/java/com/kravchenkovadim/movies/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.kravchenkovadim.movies.activities;
 
+import android.app.DownloadManager;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,10 +8,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.kravchenkovadim.movies.R;
+import com.kravchenkovadim.movies.data.MovieAdapter;
+import com.kravchenkovadim.movies.model.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private MovieAdapter movieAdapter;
+    private ArrayList<Movie> movies;
+
+    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +45,52 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.hasFixedSize();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        movies = new ArrayList<>();
+        requestQueue = Volley.newRequestQueue(this);
+        getMoview();
+    }
+
+    private void getMoview() {
+
+        String url = "https://www.omdbapi.com/?s=superman&apikey=7b05243a";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject jsonObject) {
+                try {
+                    JSONArray jsonArray = jsonObject.getJSONArray("Search");
+
+                    for(int i =0; i<jsonArray.length();i++){
+                        JSONObject jsonObject1 = jsonArray.getJSONObject(i);
+
+                        String title = jsonObject.getString("Title");
+                        String year = jsonObject.getString("Year");
+                        String posterUrl = jsonObject.getString("Poster");
+
+                        Movie movie = new Movie();
+                        movie.setTitle(title);
+                        movie.setYear(year);
+                        movie.setPosterUrl(posterUrl);
+                    }
+
+                    movieAdapter = new MovieAdapter(MainActivity.this, movies);
+                    recyclerView.setAdapter(movieAdapter);
+
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                volleyError.printStackTrace();
+            }
+        });
+        requestQueue.add(request);
     }
 }
